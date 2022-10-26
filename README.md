75c5c23ef8c37dc4bfe154eeb39118ff470acf7f
YAMASHITA,Junji <ysiijj+github@gmail.com>
2022-10-27 00:35:07 +0900

    ruby_lang_docs_version_switcher.user.js を追加 バージョンセレクタ イメージを追加

--- README.md
+++ README.md
@@ -11,11 +11,27 @@ Emacs 28.2 版でのみ表示確認しています。
 
 HTML 構造に強く依存するため、旧版・今後の新版で同様に使えるかどうかは不明です。
 
+## ruby_lang_docs_version_switcher.user.js
+
+![バージョンセレクタ](/assets/images/ruby_lang_docs_version_switcher.png)
+
+[Ruby リファレンスマニュアル](https://www.ruby-lang.org/ja/) で表示するバージョンを選択可能にするものです。
+
+バージョンの選択肢は固定で埋め込んでいるので、好みに応じて修正して使用してください。
+
+URL 中のバージョンを書き換えてリンクを作成しているだけなので
+切り替え後のバージョンに存在しないページは Not Found エラーページが表示されます。
+
 ## ruby_on_rails_api_version_switcher.user.js
+
+![バージョンセレクタ](/assets/images/ruby_on_rails_api_version_switcher.png)
 
 [Ruby on Rails API](https://api.rubyonrails.org/) で表示するバージョンを選択可能にするものです。
 
 バージョンの選択肢は固定で埋め込んでいるので、好みに応じて修正して使用してください。
+
+URL 中のバージョンを書き換えてリンクを作成しているだけなので
+切り替え後のバージョンに存在しないページは Edge ページにリダイレクトされます。
 
 ### 6.0 以前について
 
---
+++ ruby_lang_docs_version_switcher.user.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name         Ruby lang docs version switch
+// @namespace    https://github.com/ysjj/userscript
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://docs.ruby-lang.org/ja/*/*.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ruby-lang.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const versionSwitcherSetup = () => {
+        let header = document.querySelector("header > nav > ol.inline-breadcrumb-list > li:first-child");
+        if (!header) return;
+        if (header.children[0]) header = header.children[0]; // anchor content
+
+        const [_, currentVersion] = location.pathname.match(new RegExp(`^/[^/]+/([^/]+)`));
+
+        const versions = document.createElement('span');
+        versions.classList.add('_userscript_ruby_lang_version_content');
+
+        [...(new Set(['2.7.0', '3.0', '3.1', currentVersion]))].sort().forEach((ver, _) => {
+            if (ver != currentVersion) {
+                const anchor = document.createElement('a');
+                anchor.innerText = ver;
+                anchor.href = location.pathname.replace(currentVersion, ver);
+                versions.append(anchor);
+            }
+        });
+
+        const version = document.createElement('span');
+        version.classList.add('_userscript_ruby_lang_version');
+        version.innerText = currentVersion;
+        version.append(versions);
+
+        const style = document.createElement('style');
+        style.innerText = `
+          ._userscript_ruby_lang_version {
+            position: relative;
+            display: inline-block;
+          }
+          ._userscript_ruby_lang_version_content {
+            display: none;
+            position: absolute;
+            background-color: white;
+            padding: 0 0.5rem;
+            left: -0.5rem;
+            z-index: 1;
+          }
+          ._userscript_ruby_lang_version_content a {
+            display: block;
+            font-weight: unset;
+            text-decoration: none;
+          }
+          ._userscript_ruby_lang_version:hover ._userscript_ruby_lang_version_content {
+            display: block;
+          }
+        `;
+
+        const text = header.innerText;
+        const index = text.indexOf(currentVersion);
+        header.replaceChildren(
+            text.substring(0, index),
+            version,
+            text.substring(index + currentVersion.length),
+            style
+        );
+    };
+
+    versionSwitcherSetup();
+})();
